f0b9b66a0a028960ab0f904c6b2b87e817ab9f42
Jithesh M <jitheshmjithooz@gmail.com>
2022-12-17 23:02:33 +0530

    Diffrent movie categories added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Banner from "./Components/Banner/Banner";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Rowpost from "./Components/RowPost/Rowpost";
-import {action} from "../src/constants/constant"
+import {action, Animation, comedy, Drama, Romance} from "../src/constants/constant"
 
 function App() {
   return (
@@ -13,7 +13,10 @@ function App() {
       <section className="container-fluid">
       <Banner/>
       <Rowpost title="Get In On the Action" url={action}/>
-      
+      <Rowpost title="Drama" url={Drama}/>
+      <Rowpost title="Romance" url={Romance}/>
+      <Rowpost title="Animantion" url={Animation}/>
+      <Rowpost title="Comedy" url={comedy}/>
       <Footer/>
       </section>
     </div>
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -3,4 +3,8 @@ export const API_KEY="6d222b2dbe49c99e839cbc0ec0843e81";
 export const baseImageURL="https://image.tmdb.org/t/p/w500/"
 
 
-export const action=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=28`
+export const action=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=28&primary_release_year=2022`
+export const comedy=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=35&primary_release_year=2022`
+export const Drama=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=18&primary_release_year=2022`
+export const Romance=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=10749&primary_release_year=2022`
+export const Animation=`discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=16&primary_release_year=2022`
